d81e587150361dbea3b857551b73ed4b7f8ba0a0
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-03-24 15:06:11 +0000

    updated index

--- index.php
+++ index.php
@@ -17,28 +17,24 @@
     <?php 
 			echo "<h1>Surface Area and Volume of a Cylinder, Using PHP in PHP</h1>";
 			echo "<h3>This program will calculate the surface area and volume of a cylinder.</h3>";
-      echo "<h3>Formulas: V= πr<sup>2</sup>h <br> SA=2πrh+2πr<sup>2</sup>"
+      echo "<h3>Formulas: SA=2πrh+2πr<sup>2<br>V= πr<sup>2</sup>h"
 		?>
     <img src="./images/SA_cylinder.png" alt="Surface Area of a Cylinder" width="75%" height="auto">
     <img src="./images/V_cylinder.png" alt="Volume of a Cylinder" width="75%" height="auto">
-		<!-- form to get side a, b, base, and height from the user -->
-    <form action="./results.php" method="post" target="results">
-      <label for="lblSideA">Side A (cm):</label>
-      <input type="text" id="side_a" placeholder="Enter the side a (cm)..." name="side_a"><br><br>
-      <label for="lblSideB">Side B (cm):</label>
-      <input type="text" id="side_b" placeholder="Enter the side b (cm)..." name="side_b"><br><br>
-      <label for=lblBase">Base (cm):</label>
-      <input type="text" id="base" placeholder="Enter the base (cm)..." name="base"><br><br>
+		<!-- form to get radius and height from the user -->
+    <form action="./answers.php" method="post" target="results">
+      <label for="lblRadius">Radius (cm):</label>
+      <input type="text" id="radius" placeholder="Enter the radius (cm)..." name="radius"><br><br>
       <label for="lblHeight">Height (cm):</label>
       <input type="text" id="height" placeholder="Enter the height (cm)..." name="height"><br><br>
 
-      <input type="submit" value="Calculate Perimeter and Area">
+      <input type="submit" value="Calculate Surface Area and Volume">
 		</form>
 
 			<!-- iframe for the results to show on the web page. -->
 			<iframe id="results" name="results">			
-				The area of the triangle is  " + $area + cm<sup>2</sup>"<br>
-        The perimeter of the triangle is " + $perimeter + cm"
+				The surface area of the cylinder is  " + $sArea + cm<sup>2</sup>"<br>
+        The perimeter of the triangle is " + $vCylinder + cm<sup>3</sup>"
 	    </iframe>
 	</body>
 </html>
