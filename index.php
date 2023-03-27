b20a6774a3a708d7710ce53c6fb848cd9382948d
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-03-27 15:30:17 +0000

    updated readme

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
     <meta name="keywords" content="immaculata, ics2o">
     <meta name="author" content="Shyla Oommen">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon[1].png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32[1].png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16[1].png">
+    <link rel="manifest" href="./favicon/site[1].webmanifest">
     <!-- Link to CSS file -->
     <link rel="stylesheet" href="./css/style.css">
     <!-- Title -->
